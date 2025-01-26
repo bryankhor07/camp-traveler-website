eb14f04277c26f3c30ab2bd98a0a578de314e2a1
Bryan Khor <bryankhor07@gmail.com>
2025-01-25 17:40:32 -0800

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Camp Traveler",
+  description: "Travel UI/UX App for Campers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Navbar />
+      <body className="relative overflow-hidden">{children}</body>
+      <Footer />
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavbarLogo from "../public/hilink-logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="flexBetween max-container
+  padding-container relative z-30 py-5"
+    >
+      <Link href="/">
+        <Image src={NavbarLogo} alt="logo" width={74} height={29} />
+        <ul className="hidden h-full gap-12 lg:flex">{["Home"]}</ul>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
